e8e4a17a542913de8a28fa903a00760b9e5fe9fb
cg <congvc98@gmail.com>
2025-03-01 00:58:17 +0700

    update queue constants

--- src/queues.constant.ts
+++ src/queues.constant.ts
@@ -1,14 +1,11 @@
 export const ExchangeNames = {
-  SEND_EMAIL: 'send-email',
-  USER_CREATED: 'user-created',
+  AUTH_NOTIFICATION_EMAIL: 'send-auth-notification-email',
 } as const satisfies Record<string, string>;
 
 export const RoutingKeys = {
-  SEND_EMAIL: 'send.email',
-  USER_CREATED: 'user.created',
+  AUTH_NOTIFICATION_EMAIL: 'auth-notification-email',
 } as const satisfies Record<string, string>;
 
 export const QueueNames = {
-  SEND_EMAIL: 'send.email.queue',
-  USER_CREATED: 'user.created.queue',
+  AUTH_NOTIFICATION_EMAIL: 'auth-notification-email-queue',
 } as const satisfies Record<string, string>;
