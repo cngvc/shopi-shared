27e320c7aa40ec2c9c08b92c2d1e169ff75d836b
cg <congvc98@gmail.com>
2025-03-01 21:49:13 +0700

    add new queue constants

--- src/queues.constant.ts
+++ src/queues.constant.ts
@@ -1,11 +1,17 @@
 export const ExchangeNames = {
   AUTH_NOTIFICATION_EMAIL: 'send-auth-notification-email',
+  USERS_BUYER_UPDATE: 'update-users-buyer',
+  USERS_STORE_UPDATE: 'update-users-store',
 } as const satisfies Record<string, string>;
 
 export const RoutingKeys = {
   AUTH_NOTIFICATION_EMAIL: 'auth-notification-email',
+  USERS_BUYER_UPDATE: 'users-buyer',
+  USERS_STORE_UPDATE: 'users-store',
 } as const satisfies Record<string, string>;
 
 export const QueueNames = {
   AUTH_NOTIFICATION_EMAIL: 'auth-notification-email-queue',
+  USERS_BUYER_UPDATE: 'users-buyer-queue',
+  USERS_STORE_UPDATE: 'users-store-queue',
 } as const satisfies Record<string, string>;
