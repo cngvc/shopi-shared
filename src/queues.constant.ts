7c7ab67fbb40b30e41f52b13b7c85976e8b59a84
cg <congvc98@gmail.com>
2025-03-03 21:01:12 +0700

    add more queue keys

--- src/queues.constant.ts
+++ src/queues.constant.ts
@@ -2,16 +2,22 @@ export const ExchangeNames = {
   AUTH_NOTIFICATION_EMAIL: 'send-auth-notification-email',
   USERS_BUYER_UPDATE: 'update-users-buyer',
   USERS_STORE_UPDATE: 'update-users-store',
+  GET_STORE_USERS: 'get-store-users',
+  CREATE_SEED_PRODUCT: 'create-seeds-product',
 } as const satisfies Record<string, string>;
 
 export const RoutingKeys = {
-  AUTH_NOTIFICATION_EMAIL: 'auth-notification-email',
-  USERS_BUYER_UPDATE: 'users-buyer',
-  USERS_STORE_UPDATE: 'users-store',
+  AUTH_NOTIFICATION_EMAIL: 'send-auth-notification-email-key',
+  USERS_BUYER_UPDATE: 'update-users-buyer-key',
+  USERS_STORE_UPDATE: 'update-users-store-key',
+  GET_STORE_USERS: 'get-store-users-key',
+  CREATE_SEED_PRODUCT: 'create-seeds-product-key',
 } as const satisfies Record<string, string>;
 
 export const QueueNames = {
   AUTH_NOTIFICATION_EMAIL: 'auth-notification-email-queue',
-  USERS_BUYER_UPDATE: 'users-buyer-queue',
-  USERS_STORE_UPDATE: 'users-store-queue',
+  USERS_BUYER_UPDATE: 'update-users-buyer-queue',
+  USERS_STORE_UPDATE: 'update-users-store-queue',
+  GET_STORE_USERS: 'get-store-users-queue',
+  CREATE_SEED_PRODUCT: 'create-seeds-product-queue',
 } as const satisfies Record<string, string>;
