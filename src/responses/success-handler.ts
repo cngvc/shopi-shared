0b4b9b20e4500c362ea7cc4e4c5540cee257f084
cg <congvc98@gmail.com>
2025-03-04 12:00:10 +0700

    fix success response structure

--- src/responses/success-handler.ts
+++ src/responses/success-handler.ts
@@ -4,32 +4,32 @@ import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 class SuccessResponse {
   message: string;
   status: number;
-  metadata: any;
+  metadata: {};
 
   constructor(
     message: string,
+    metadata = {},
     status = StatusCodes.OK,
-    reasonStatusCode = ReasonPhrases.OK,
-    metadata = {}
+    reasonStatusCode = ReasonPhrases.OK
   ) {
     this.message = message ? message : reasonStatusCode;
     this.status = status;
     this.metadata = metadata;
   }
 
-  send(res: Response, headers = {}) {
+  send = (res: Response, headers = {}) => {
     return res.status(this.status).json(this);
-  }
+  };
 }
 
 export class OkRequestSuccess extends SuccessResponse {
-  constructor(message: string, metadata: any) {
+  constructor(message: string, metadata: {}) {
     super(message, metadata);
   }
 }
 
 export class CreatedRequestSuccess extends SuccessResponse {
-  constructor(message: string, metadata: any) {
-    super(message, StatusCodes.CREATED, ReasonPhrases.CREATED, metadata);
+  constructor(message: string, metadata: {}) {
+    super(message, metadata, StatusCodes.CREATED, ReasonPhrases.CREATED);
   }
 }
